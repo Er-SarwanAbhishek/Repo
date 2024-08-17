44e2ca363eefee5f3d03953dba1eb1560823ea1f
Abhishek <abhishek123@gmail.com>
2024-08-17 17:48:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Banner from './componenets/banner/Banner';
 import Features from './componenets/features/Features';
@@ -8,9 +9,13 @@ function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText">
        <div className='w-[100%] px-16'>
-        <Navbar />
-        <Banner />
-        <Features />
+        <BrowserRouter>
+        <Routes>
+          <Route path = "/" element= {<Navbar />} />
+          <Route path = "/banner" element= {<Banner />} />
+          <Route path = "/features" element= {<Features />} />
+        </Routes>
+        </BrowserRouter>
        </div>
     </div>
   );
